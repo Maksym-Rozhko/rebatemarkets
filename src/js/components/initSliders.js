1263188245d264b3faac68e2d1d132edd0b4203f
Maksym <maksymRozhko7@gmail.com>
2023-01-19 16:40:09 +0200

    education, adaptive, slider, fixes

--- src/js/components/initSliders.js
+++ src/js/components/initSliders.js
@@ -21,32 +21,29 @@ new Swiper('.swiper.main-slider', {
     },
 });
 
+new Swiper('.swiper.education__slider', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
 
-// new Swiper('.swiper.rating__slider', {
-//     slidesPerView: 4.5,
-//     spaceBetween: 20,
-//     loop: true,
+    navigation: {
+        nextEl: '.education__slider-btns .swiper-button-next',
+        prevEl: '.education__slider-btns .swiper-button-prev',
+    },
 
+    pagination: {
+        el: '.education__slider-controls.swiper-pagination',
+        clickable: true,
+    },
 
-//     breakpoints: {
-//       320: {
-//         slidesPerView: 1,
-//       },
-//       375: {
-//         slidesPerView: 1.5,
-//         spaceBetween: 10,
-//       },
-//       575: {
-//         slidesPerView: 2.5,
-//         spaceBetween: 10,
-//       },
-//       1024: {
-//         slidesPerView: 3.5,
-//         spaceBetween: 10,
-//       },
-//       1440: {
-//         slidesPerView: 4.5,
-//         spaceBetween: 20,
-//     }
-//     },
-// });
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+});
