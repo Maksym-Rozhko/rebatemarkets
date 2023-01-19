8260d46e744e86f6f29a8e73062e76d76344c9c7
Maksym <maksymRozhko7@gmail.com>
2023-01-19 11:08:59 +0200

    main slider, adaptive, fixes

--- src/js/components/initSliders.js
+++ src/js/components/initSliders.js
@@ -1,35 +1,52 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay]);
 
-new Swiper('.swiper.rating__slider', {
-    slidesPerView: 4.5,
-    spaceBetween: 20,
+new Swiper('.swiper.main-slider', {
+    slidesPerView: 1,
+    speed: 1000,
     loop: true,
-
-    navigation: {
-        nextEl: '.slider__controls .rating__btns.swiper-button-next',
-        prevEl: '.slider__controls .rating__btns.swiper-button-prev',
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
     },
 
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-      375: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
-      },
-      575: {
-        slidesPerView: 2.5,
-        spaceBetween: 10,
-      },
-      1024: {
-        slidesPerView: 3.5,
-        spaceBetween: 10,
-      },
-      1440: {
-        slidesPerView: 4.5,
-        spaceBetween: 20,
-      }
-  },
+    navigation: {
+        nextEl: '.main-slider__btns .swiper-button-next',
+        prevEl: '.main-slider__btns .swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.main-slider__controls.swiper-pagination',
+        clickable: true,
+    },
 });
+
+
+// new Swiper('.swiper.rating__slider', {
+//     slidesPerView: 4.5,
+//     spaceBetween: 20,
+//     loop: true,
+
+
+//     breakpoints: {
+//       320: {
+//         slidesPerView: 1,
+//       },
+//       375: {
+//         slidesPerView: 1.5,
+//         spaceBetween: 10,
+//       },
+//       575: {
+//         slidesPerView: 2.5,
+//         spaceBetween: 10,
+//       },
+//       1024: {
+//         slidesPerView: 3.5,
+//         spaceBetween: 10,
+//       },
+//       1440: {
+//         slidesPerView: 4.5,
+//         spaceBetween: 20,
+//     }
+//     },
+// });
