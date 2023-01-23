798da6a6ec81d7b02d87409fa1a975818daba029
Maksym <maksymRozhko7@gmail.com>
2023-01-23 12:00:58 +0200

    scroll top button

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,4 +1,4 @@
 import initSliders from './components/initSliders';
 import tabs from './components/tabs';
 // import headerScrollFixed from './components/fixedHeader';
-// import scrollToTop from './components/scrollToTop';
+import scrollToTop from './components/scrollToTop';
